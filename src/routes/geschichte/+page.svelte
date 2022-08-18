8cd5b89bfb81541af2349f92c0c291068ae553dc
Markus Garnitz <garnitz@bayern.info>
2022-08-18 08:10:20 +0200

    initial commit

---
+++ src/routes/geschichte/+page.svelte
@@ -0,0 +1,40 @@
+<section class="py-20 bg-white">
+    <div class="flex flex-col px-8 mx-auto space-y-12 max-w-7xl xl:px-12">
+        <div class="relative">
+            <h2 class="w-full text-3xl font-bold text-center sm:text-4xl md:text-5xl"> Level Up Your Designs</h2>
+            <p class="w-full py-8 mx-auto -mt-2 text-lg text-center text-gray-700 intro sm:max-w-3xl">Add some nice touches to your interface with our latest designs, components, and templates. We've crafted a beautiful user experience that your visitors will love. </p>
+        </div>
+        <ol class="border-l border-gray-300">
+            <li>
+            <div class="flex flex-start items-center pt-3">
+                <div class="bg-gray-300 w-2 h-2 rounded-full -ml-1 mr-3"></div>
+                <p class="text-gray-500 text-sm">01.07.2021</p>
+            </div>
+            <div class="mt-0.5 ml-4 mb-6">
+                <h4 class="text-gray-800 font-semibold text-xl mb-1.5">Title of section 1</h4>
+                <p class="text-gray-500 mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
+            </div>
+            </li>
+            <li>
+            <div class="flex flex-start items-center pt-2">
+                <div class="bg-gray-300 w-2 h-2 rounded-full -ml-1 mr-3"></div>
+                <p class="text-gray-500 text-sm">13.09.2021</p>
+            </div>
+            <div class="mt-0.5 ml-4 mb-6">
+                <h4 class="text-gray-800 font-semibold text-xl mb-1.5">Title of section 2</h4>
+                <p class="text-gray-500 mb-3">Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error architecto recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam an deleniti.</p>
+            </div>
+            </li>
+            <li>
+            <div class="flex flex-start items-center pt-2">
+                <div class="bg-gray-300 w-2 h-2 rounded-full -ml-1 mr-3"></div>
+                <p class="text-gray-500 text-sm">25.11.2021</p>
+            </div>
+            <div class="mt-0.5 ml-4 pb-5">
+                <h4 class="text-gray-800 font-semibold text-xl mb-1.5">Title of section 3</h4>
+                <p class="text-gray-500 mb-3">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+            </div>
+            </li>
+        </ol>
+    </div>
+</section>
