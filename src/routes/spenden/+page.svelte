1f5bf497c72a31e8bfb12bea8b5b1f8fe90ad486
Markus Garnitz <garnitz@bayern.info>
2024-03-14 10:06:35 +0100

    Update Summe & Datum

--- src/routes/spenden/+page.svelte
+++ src/routes/spenden/+page.svelte
@@ -61,8 +61,8 @@
 
 			<section class="antialiased text-gray-600">
 				<div class="flex flex-col place-items-center">
-					<h1 class="font-bold text-2xl">Aktuelle Spendensumme: 388.302,28 €</h1>
-					<div class="mb-2">(Stand: 31.01.2024)</div>
+					<h1 class="font-bold text-2xl">Aktuelle Spendensumme:  402.424,72 €</h1>
+					<div class="mb-2">(Stand: 14.03.2024)</div>
 
 					<div
 						class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200"
